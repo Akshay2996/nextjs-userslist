70c36704f66097ee7e92aee533dcbfbee5f6c5ba
Akshay Sharma <akshaysharmaay@gmail.com>
2021-02-22 22:28:39 +0530

    Added about page

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,11 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+const About = () => {
 	return (
 		<div>
-			<h1>HomePage</h1>
+			<h1>About</h1>
 			<p>lorem ipsum dolor sit amet, consectetur adip</p>
 			<p>lorem ipsum dolor sit amet, consectetur</p>
 		</div>
 	);
-}
+};
+
+export default About;

634b7d9bc91a249023cc939cf4336bb1c4099f44
Akshay Sharma <akshaysharmaay@gmail.com>
2021-02-23 19:15:31 +0530

    Added Link to navigate to different pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,22 @@
+import Link from "next/link";
+
 const Navbar = () => {
-	return (
-		<nav>
-			<div className="logo">
-				<h1>Users List</h1>
-			</div>
-			<a>Home</a>
-			<a>About</a>
-			<a>User Listing</a>
-		</nav>
-	);
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Users List</h1>
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/users">
+        <a>User Listing</a>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import Head from "next/head";
+import Link from "next/link";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-	return (
-		<div>
-			<Navbar />
-			<h1>HomePage</h1>
-			<p>lorem ipsum dolor sit amet, consectetur adip</p>
-			<p>lorem ipsum dolor sit amet, consectetur</p>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <h1>HomePage</h1>
+      <p>lorem ipsum dolor sit amet, consectetur adip</p>
+      <p>lorem ipsum dolor sit amet, consectetur</p>
+      <Link href="/users">
+        <a>See User Listing</a>
+      </Link>
+      <Footer />
+    </div>
+  );
 }

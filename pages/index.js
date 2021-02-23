08231ec678a3111b1b92f6bf655888865bf3aa02
Akshay Sharma <akshaysharmaay@gmail.com>
2021-02-23 19:32:52 +0530

    Added Layout component that wraps around all other pages & components

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,16 @@
 import Head from "next/head";
 import Link from "next/link";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>HomePage</h1>
       <p>lorem ipsum dolor sit amet, consectetur adip</p>
       <p>lorem ipsum dolor sit amet, consectetur</p>
       <Link href="/users">
         <a>See User Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }

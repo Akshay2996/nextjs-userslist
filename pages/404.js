681c4f21a1eaa7fa0b50978ebbc0540681cb7aa8
Akshay Sharma <akshsharma@paypal.com>
2021-02-25 13:25:55 +0530

    Redirecting user from 404 page automatically to homepage

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+	const router = useRouter();
+
+	useEffect(() => {
+		setTimeout(() => {
+			router.push("/");
+		}, 4000);
+	}, []);
+
 	return (
 		<div className="not-found">
 			<h1>404 - Error</h1>

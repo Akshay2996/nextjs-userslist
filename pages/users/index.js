e91579ee2628caeae53bae28a2177370ccf33b83
Akshay Sharma <akshsharma@paypal.com>
2021-02-25 17:14:34 +0530

    Fetch data using getStaticProps method in NextJS

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head";
+import styles from "../../styles/Users.module.css";
 
-const Users = () => {
+// This function runs at buildtime
+export const getStaticProps = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
+
+	return {
+		props: { users: data },
+	};
+};
+
+const Users = ({ users }) => {
 	return (
 		<>
 			<Head>
@@ -9,6 +20,13 @@ const Users = () => {
 			</Head>
 			<div>
 				<h1>All Users</h1>
+				{users.map((user) => (
+					<div key={user.id}>
+						<a className={styles.single}>
+							<h3>{user.name}</h3>
+						</a>
+					</div>
+				))}
 			</div>
 		</>
 	);

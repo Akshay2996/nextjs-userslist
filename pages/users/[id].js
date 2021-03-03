b8d8a3ba0d595992a760aa11eb49662123c74566
Akshay Sharma <akshsharma@paypal.com>
2021-03-03 10:19:26 +0530

    Fetching single item using getStaticPaths & getStaticProps method

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -15,10 +15,24 @@ export const getStaticPaths = async () => {
 	};
 };
 
-const Details = () => {
+// This function runs at buildtime & fetches the individual user data
+export const getStaticProps = async (context) => {
+	const id = context.params.id;
+	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+	const data = await res.json();
+
+	return {
+		props: { user: data },
+	};
+};
+
+const Details = ({ user }) => {
 	return (
 		<div>
-			<h1>Details Page</h1>
+			<h1>{user.name}</h1>
+			<p>{user.email}</p>
+			<p>{user.website}</p>
+			<p>{user.address.city}</p>
 		</div>
 	);
 };
